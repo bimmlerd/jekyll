9f6aa171759c3332cd1b1a09062e73c07480610f
David Bimmler <bimmlerd@student.ethz.ch>
2015-09-24 14:35:59 +0200

    hw1 initial commit

---
+++ HW1/src/cd/backend/codegen/AstCodeGenerator.java
@@ -0,0 +1,46 @@
+package cd.backend.codegen;
+
+import java.io.Writer;
+import java.util.List;
+
+import cd.Main;
+import cd.ir.Ast.ClassDecl;
+
+public class AstCodeGenerator {
+
+	protected ExprGenerator eg;
+	protected StmtGenerator sg;
+	
+	protected final Main main;
+	
+	protected final AssemblyEmitter emit;
+	protected final RegisterManager rm = new RegisterManager();
+
+	AstCodeGenerator(Main main, Writer out) {
+		this.emit = new AssemblyEmitter(out);
+		this.main = main;
+		this.eg = new ExprGenerator(this);
+		this.sg = new StmtGenerator(this);
+	}
+
+	protected void debug(String format, Object... args) {
+		this.main.debug(format, args);
+	}
+
+	public static AstCodeGenerator createCodeGenerator(Main main, Writer out) {
+		return new AstCodeGenerator(main, out);
+	}
+	
+	
+	/**
+	 * Main method. Causes us to emit x86 assembly corresponding to {@code ast}
+	 * into {@code file}. Throws a {@link RuntimeException} should any I/O error
+	 * occur
+	 */
+	public void go(List<? extends ClassDecl> astRoots) {
+		for (ClassDecl ast : astRoots) {
+			sg.gen(ast);
+		}
+	}
+
+}
