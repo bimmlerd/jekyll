90d770814639295435bd368597ab10a203859ab6
David Bimmler <bimmlerd@student.ethz.ch>
2015-10-09 15:00:06 +0200

    HW2 initial commit

---
+++ HW2/src/cd/frontend/parser/JavaliAstVisitor.java
@@ -0,0 +1,20 @@
+package cd.frontend.parser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cd.ToDoException;
+import cd.ir.Ast.ClassDecl;
+
+public final class JavaliAstVisitor extends JavaliBaseVisitor<Void> {
+	
+	public List<ClassDecl> classDecls = new ArrayList<>();
+	@Override
+	public Void visitClassDecl(ClassDeclContext ctx) {
+		{
+			// classDecls = ...;
+			throw new ToDoException();
+		}
+		return null;
+	}
+}
