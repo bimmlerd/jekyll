f7536db9211437d18daa877a4544baf1acde2ac1
Andrina <adenzler@student.ethz.ch>
2015-12-17 10:05:07 +0100

    renaming, comments and stuff

--- HW5/src/cd/analysis/BexprDataFlow.java
+++ HW5/src/cd/analysis/BexprDataFlow.java
@@ -3,6 +3,7 @@ package cd.analysis;
 import cd.ir.Ast;
 import cd.ir.Ast.Expr;
 import cd.ir.AstVisitor;
+import cd.ir.ExprVisitor;
 import cd.util.debug.AstOneLine;
 
 import java.util.HashSet;
@@ -13,34 +14,36 @@ import java.util.Set;
  * these expressions (e.g., expression line number with the expression string or AST subtree).
  */
 public class BexprDataFlow extends BwdAndDataFlow<Expr> {
+    private final ExpressionCollector exprCollector = new ExpressionCollector();
+    private final SetInitializer setInitializer = new SetInitializer();
+    private final ContainsVisitor containsVisitor = new ContainsVisitor();
+
     @Override
     void initSets(ControlFlowGraph cfg) {
-        ExpressionCollector exprCollector = new ExpressionCollector();
-
         // collect all expressions used intra-procedural
-        Set<Expr> exprs = new HashSet<>();
+        Set<Expr> allExpr = new HashSet<>();
         for (BasicBlock b : cfg.blockSet) {
             for (Ast ast : b.statements) {
-                exprs.addAll(exprCollector.visit(ast, null));
+                allExpr.addAll(exprCollector.visit(ast, null));
             }
         }
         setSolution(startPoint(cfg), new HashSet<>()); // the solution set of the start point is empty
 
         for (BasicBlock b : cfg.blockSet) {
-            setSolution(b, exprs); // the solution sets for all other blocks initially contain all expressions
-            DFContext ctx = new DFContext(exprCollector, exprs);
-            DFVisitor dfVisitor = new DFVisitor();
+            setSolution(b, allExpr); // the solution sets for all other blocks initially contain all expressions
+            LocalSetContext ctx = new LocalSetContext(allExpr);
             for (Ast ast : b.statements) {
-                ctx = dfVisitor.visit(ast, ctx);
+                ctx = setInitializer.visit(ast, ctx);
             }
-            b.localCut.addAll(ctx.localCut);
             b.localNew.addAll(ctx.localNew);
+            b.localCut.addAll(ctx.localCut);
         }
     }
 
     @Override
     void evaluateDataFlow(ControlFlowGraph cfg) {
         for (BasicBlock b : cfg.blockSet) {
+            // report busy expressions at the start of an if/else or while statement
             if (b.condition != null && !context(b).isEmpty()) {
                 System.out.println(String.format("Busy expressions at %s in method %s.%s:",
                         AstOneLine.toString(b.condition), cfg.classDecl.name, cfg.methodDecl.name));
@@ -49,6 +52,9 @@ public class BexprDataFlow extends BwdAndDataFlow<Expr> {
         }
     }
 
+    /**
+     * Visits statements and returns the set of expressions we are interested in.
+     */
     // TODO: we don't want expressions that contain arrays or fields
     protected class ExpressionCollector extends AstVisitor<Set<Expr>, Void> {
         @Override
@@ -68,20 +74,62 @@ public class BexprDataFlow extends BwdAndDataFlow<Expr> {
         }
     }
 
-    private class DFVisitor extends AstVisitor<DFContext, DFContext> {
+    /**
+     * Needed to initialize the localNew and localCut for each basic block.
+     */
+    private class SetInitializer extends AstVisitor<LocalSetContext, LocalSetContext> {
         @Override
-        public DFContext assign(Ast.Assign ast, DFContext arg) {
+        public LocalSetContext assign(Ast.Assign ast, LocalSetContext arg) {
             if (ast.left() instanceof Ast.Var) {
-                arg.cutExpressionsContaining((Ast.Var) ast.left());
+                arg.updateLocalCutWithExprContaining((Ast.Var) ast.left());
             }
-            arg.filterCut(arg.expressionCollector.visit(ast.right(), null));
+            visit(ast.right(), arg);
             return arg;
         }
 
         @Override
-        protected DFContext dflt(Ast ast, DFContext arg) {
-            arg.filterCut(arg.expressionCollector.visit(ast, null));
+        protected LocalSetContext dflt(Ast ast, LocalSetContext arg) {
+            arg.updateLocalNewWithExprNotInCut(exprCollector.visit(ast, null));
             return arg;
+        }
+    }
+
+    /**
+     * Checks whether a given expressions contains a specific variable.
+     */
+    private class ContainsVisitor extends ExprVisitor<Boolean, Ast.Var> {
+        @Override
+        public Boolean var(Ast.Var ast, Ast.Var arg) {
+            return ast.equals(arg);
+        }
+
+        @Override
+        public Boolean visitChildren(Ast.Expr ast, Ast.Var arg) {
+            Boolean result = Boolean.FALSE;
+            for (Ast child : ast.children())
+                result |= visit((Ast.Expr)child, arg);
+            return result;
+        }
+    }
+
+    /**
+     * Holds intermediate localNew and localCut for the initialization process.
+     */
+    private class LocalSetContext {
+        protected final Set<Ast.Expr> localNew = new HashSet<>();
+        protected final Set<Ast.Expr> localCut = new HashSet<>();
+        protected final Set<Ast.Expr> allExpr;
+
+        public LocalSetContext(Set<Ast.Expr> allExpr) {
+            this.allExpr = allExpr;
+        }
+
+        void updateLocalCutWithExprContaining(Ast.Var var) {
+            allExpr.stream().filter(expr -> containsVisitor.visit(expr, var)).forEach(localCut::add);
+        }
+
+        void updateLocalNewWithExprNotInCut(Set<Ast.Expr> exprs) {
+            exprs.stream().filter(expr -> !localCut.contains(expr)).forEach(localNew::add);
         }
     }
 }
